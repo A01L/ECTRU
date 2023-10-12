cea8e6aa2366def0aed22b767f64d7c901740cfd
A01L <100783130+A01L@users.noreply.github.com>
2023-10-12 23:20:10 +0600

    v0.6

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "ectr/main.php";
+require_once "ectr/medix.php";
+
+echo "ECTR success started!";
+?>
---
+++ view/error_pages/404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>ECTR - 404 </title>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:100,400' rel='stylesheet' type='text/css'>
+
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="<?php echo($ectr_ns.'/public/css/ectr.css'); ?>">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="wrapper">
+  <h1>404</h1>
+  <p>Эта страница была удалена или перемещена...</p>
+  <div class="buttons"><a href="#" onclick="window.history.go(-1); return false;">Назад</a><a href="<?php echo locate(); ?>">На главную</a><br/></div>
+</div>
+<div class="space">
+  <div class="blackhole"></div>
+  <div class="ship"></div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
